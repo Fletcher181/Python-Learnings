b52fb96e79d63cd3b82c3edd70e8cdfc609ee4be
Fletcher181 <fletchermalazarte18@gmail.com>
2025-11-04 10:53:55 +0800

    Modified the object usage. Implemented ProductManager to use the CRUD functions.

--- MiniTasks/Product.py
+++ MiniTasks/Product.py
@@ -1,8 +1,3 @@
-def viewProducts(product_list):
-    for product in product_list:
-        print(f"{product.id}. {product.name}(P{product.price})   Qty: {product.quantity}")
-    
-
 class Product:
     def __init__(self, id, name, price, quantity):
         self.id = id
@@ -10,33 +5,35 @@ class Product:
         self.price = price
         self.quantity = quantity
 
-    def addProduct(self, product_list):
-        product_list.append(self)
+class ProductManager:
+    def __init__(self):
+        self.product_list = []
+
+    def addProduct(self, product):
+        self.product_list.append(product)
         print("You have successfully added the product!")
 
-
-    def removeProduct(self, product_list, target_id):
-        for product in product_list:
+    def removeProduct(self, target_id):
+        for product in self.product_list:
             if product.id == target_id:
-                product_list.remove(product)
-                print("You have successfully removed the product!")
-
-    def updateProduct(self, product_list, target_id, new_name, new_price, new_quantity):
-        for product in product_list:
+                self.product_list.remove(product)
+        print("You have successfully removed the product!")
+    
+    def updateProduct(self, target_id, new_name, new_price, new_quantity):
+        for product in self.product_list:
             if product.id == target_id:
                 product.name = new_name
                 product.price = new_price
                 product.quantity = new_quantity
-        print("You have succussfully updated the product!")
-    
+        print("You have successfully updated the product!")
 
-
+    def viewProducts(self):
+        for product in self.product_list:
+            print(f"{product.id}. {product.name} (P{product.price}) Qty: {product.quantity}")
 
 if __name__ == "__main__":
-
     product_id = 0
-    product_list = []
-    
+    manager = ProductManager()
     print("Fletchy's Product Management")
 
     while True:
@@ -54,31 +51,28 @@ if __name__ == "__main__":
                 product_price = float(input("Enter product price: "))
                 product_quantity = int(input("Enter product quantity: "))
 
-                p1 = Product(product_id, product_name, product_price, product_quantity)
-                p1.addProduct(product_list)
+                new_product = Product(product_id, product_name, product_price, product_quantity)
+                manager.addProduct(new_product)
             case 2:
                 print("\nProduct List")
-                viewProducts(product_list)
+                manager.viewProducts()
                 target_id = int(input("Enter id to delete: "))
-                p1.removeProduct(product_list, target_id)
+                manager.removeProduct(target_id)
             case 3:
                 print("\nProduct List")
-                viewProducts(product_list)
+                manager.viewProducts()
                 target_id = int(input("Enter id to update: "))
-                product_name = input("Enter product name: ")
-                product_price = float(input("Enter product price: "))
-                product_quantity = int(input("Enter product quantity: "))
-                p1.updateProduct(product_list, target_id, product_name, product_price, product_quantity)
+                new_name = input("Enter product name: ")
+                new_price = float(input("Enter product price: "))
+                new_quantity = int(input("Enter product quantity: "))
+                manager.updateProduct(target_id, new_name, new_price, new_quantity)
             case 4:
                 print("\nProduct List")
-                viewProducts(product_list)
+                manager.viewProducts()
             case 5:
                 print("\nThank you for using!")
                 break
             case _:
                 print("\nInvalid option!")
                 continue
-
-
-
 
