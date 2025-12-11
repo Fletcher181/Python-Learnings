28aa7963e3d44e1724f9ae2dc5e23d9a40827ba6
Fletcher181 <fletchermalazarte18@gmail.com>
2025-12-11 10:50:20 +0800

    Worked on add to cart function

--- ProgLang-FINAL/main.py
+++ ProgLang-FINAL/main.py
@@ -1,16 +1,34 @@
 import tkinter as tk
 
+menu = {
+    "Ramen": {
+        "Shoyu": 349,
+        "Spicy Miso": 369,
+        "Tonkotsu": 399
+    },
+    "Donburi": {
+        "Beef": 180,
+        "Chicken": 170,
+        "Vegetable": 150
+    },
+    "Others": {
+        "Beef": 180,
+        "Chicken": 170,
+        "Vegetable": 150
+    }
+}
+
+cart = {}
+
+
 root = tk.Tk()
 
 root.geometry("1080x1920")
 
 landingPage = tk.Frame(root)
+orderTypePage = tk.Frame(root)
 orderPage = tk.Frame(root)
 
-orderTypePage = tk.Frame(root)
-orderRamenPage = tk.Frame(root)
-orderDonburiPage = tk.Frame(root)
-orderOthersPage = tk.Frame(root)
 
 landingPage.pack(fill="both", expand=True)
 
@@ -28,34 +46,32 @@ def go_to_order_type():
 btnStart = tk.Button(landingPage, text="TAP TO START", font=('Baloo Tammudu', 80), command=go_to_order_type)
 btnStart.pack()
 
-def show_ramen_items():
-    menu = {
-        "Ramen": {
-            "Shoyu": 349,
-            "Spicy Miso": 369,
-            "Tonkotsu": 399
-        },
-        "Donburi": {
-            "Beef": 180,
-            "Chicken": 170,
-            "Vegetable": 150
-        },
-        "Others": {
-            "Beef": 180,
-            "Chicken": 170,
-            "Vegetable": 150
+def add_to_cart(item, price):
+    if item in cart:
+        cart[item] += 1
+    else:
+        cart[item] = {
+            "price": price,
+            "quantity": 1
         }
-    }
+
+def show_items(section):
+    for widget in orderPage.winfo_children():
+        widget.destroy()
+
+    for item, price in menu[section].items():
+        btn = tk.Button(orderPage, text=f"{item}\n{price}", font=('Baloo Tammudu', 45), command=lambda i=item, p=price: add_to_cart(i, p))
+        btn.pack()
+        
+
 
 def go_to_order():
     orderTypePage.pack_forget()
     orderPage.pack(fill="both", expand=True)
 
-    sections = ['Ramen', 'Donburi', 'Others']
-
-    btnRamen = tk.Button(orderPage, text="RAMEN", font=('Baloo Tammudu', 45), command=show_ramen_items)
-    btnDonburi = tk.Button(orderPage, text="DONBURI", font=('Baloo Tammudu', 45))
-    btnOthers = tk.Button(orderPage, text="OTHERS", font=('Baloo Tammudu', 45))
+    btnRamen = tk.Button(orderPage, text="RAMEN", font=('Baloo Tammudu', 45), command=lambda: show_items("Ramen"))
+    btnDonburi = tk.Button(orderPage, text="DONBURI", font=('Baloo Tammudu', 45), command=lambda: show_items("Donburi"))
+    btnOthers = tk.Button(orderPage, text="OTHERS", font=('Baloo Tammudu', 45), command=lambda: show_items("Others"))
 
     btnRamen.pack()
     btnDonburi.pack()
